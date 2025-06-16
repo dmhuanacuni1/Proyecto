5a7782147fd45d5c6c7c157a5d034950b683256e
Dani <dmhuanacuni@umsa.bo>
2025-06-16 00:42:58 -0400

    arreglo del la base

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Principal from './Principal.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/Proyecto'>
     <Principal></Principal>
   </BrowserRouter>
 )
